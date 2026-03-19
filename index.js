15bfe78c6698ba919204548f39d52fa0d7931341
strandedorca <bestofmau@gmail.com>
2026-03-19 23:52:41 +1100

    Add POST route

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+const DATA_FILE = path.join(__dirname, "words.json");
 
 // Create an Express application
 const app = express();
-const PORT = 3000;
+// Middleware to parse JSON bodies
+// This reads the raw request body and attaches the parsed JSON to req.body
+// Without this, req.body would be undefined
+// Middleware order matters!
+app.use(express.json());
 
-// My list of hard coded words and their definitions
-const words = [
-  {
-    id: 1,
-    word: "malinger",
-    definition: "to pretend to be sick in order to avoid doing work",
-    example:
-      "He was accused of malingering to avoid his responsibilities at work.",
-    createdAt: "2024-06-01T12:00:00Z",
-  },
-  {
-    id: 2,
-    word: "serendipity",
-    definition:
-      "the occurrence of events by chance in a happy or beneficial way",
-    example: "Finding that old book in the library was pure serendipity.",
-    createdAt: "2024-06-01T12:05:00Z",
-  },
-  {
-    id: 3,
-    word: "iterate",
-    definition: "to perform or utter repeatedly",
-    example:
-      "The software development process often involves iterating on the design.",
-    createdAt: "2024-06-01T12:10:00Z",
-  },
-];
+function readWordsFromFile() {
+  const data = fs.readFileSync(DATA_FILE, "utf-8");
+  return JSON.parse(data);
+}
+
+function writeWordsToFile(words) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(words, null, 2));
+}
 
 // Define a simple route
 app.get("/", (req, res) => {
@@ -39,17 +29,41 @@ app.get("/", (req, res) => {
 
 // Route to get all words in the dictionary
 app.get("/words", (req, res) => {
+  // res.json(words);
+  const words = readWordsFromFile();
   res.json(words);
 });
 
 app.get("/words/:id", (req, res) => {
+  const words = readWordsFromFile();
   const id = Number(req.params.id);
   const word = words.find((w) => w.id === id);
+
   if (!word) {
     return res.status(404).json({ error: "Word not found" });
   }
 
   res.json(word);
+});
+
+app.post("/words", (req, res) => {
+  const words = readWordsFromFile();
+  const { word, definition, example } = req.body;
+  if (!word || !definition) {
+    return res.status(400).json({ error: "Word and definition are required" });
+  }
+
+  const newWord = {
+    id: words.length + 1,
+    word,
+    definition,
+    example: example || "",
+    createdAt: new Date().toISOString(),
+  };
+
+  words.push(newWord);
+  writeWordsToFile(words);
+  res.status(201).json(newWord);
 });
 
 // Starts the server and listens on the specified port
